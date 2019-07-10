d9642c37a6d4e3f718e8a73e287e2616ab66f46b
arrianne <arrianneoshea@hotmail.co.uk>
2019-07-10 10:58:14 +0100

    creating the stocks component which uses a bootstrap template and currently presents some dummy data

--- src/components/Stocks/Stock.vue
+++ src/components/Stocks/Stock.vue
@@ -1,3 +1,30 @@
-<template id="">
-  <h1>The Stock Component</h1>
+<template>
+    <div class="col-sm-6 col-md-4">
+        <div class="panel panel-success">
+            <div class="panel-heading">
+                <h3 class="panel-title">
+
+                    <small>(Price: PRICE)</small>
+                </h3>
+            </div>
+            <div class="panel-body">
+                <div class="pull-left">
+                    <input
+                            type="number"
+                            class="form-control"
+                            placeholder="Quantity"
+
+                    >
+                </div>
+                <div class="pull-right">
+                    <button
+                            class="btn btn-success"
+
+                            
+                    >BUY
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
